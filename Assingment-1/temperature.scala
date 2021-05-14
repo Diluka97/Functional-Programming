fab573f10664a7769a0a8b779310f6d7d966752b
Diluka97 <dilukadeshan97@gmail.com>
2021-05-14 20:26:54 +0530

    1 and 2 questions completed

---
+++ Assingment-1/temperature.scala
@@ -0,0 +1,9 @@
+object temperature{
+		
+		def main(args:Array[String]){
+		}
+		
+		def fahre(x:Double)= x*1.8+32;
+		
+		println("35C = " + fahre(35) + "F");
+	}
---
+++ Assingment-1/volume.scala
@@ -0,0 +1,9 @@
+object volume{
+		def main(args:Array[String])
+			{
+			}
+			
+		def vol(r:Double)=(4 * 3.14 * r*r*r)/3;
+
+		println("volume is " + vol(5));
+	}
